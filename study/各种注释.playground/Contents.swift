39413e955f3bb97f113accfdfd808dfb3d16a91b
xifanGame <xifanGame@163.com>
2015-07-29 17:42:35 +0800

    playground添加文档图片说明
    playground添加文档图片说明

---
+++ study/各种注释.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//:单行注释
+
+
+/*: 多行注释 这行不显示
+# 标题1
+内容1
+# 标题2
+内容2
+*/
+
+//:[点击打开链接](http://www.baidu.com)
+
+
+/*: 树型
+1. 标题1
+2. 标题2
+3. 标题3
+*/
+
+/*: 横线
+
+记得有换行
+
+----
+
+记得有换行
+*/
+
+//: 这是**粗体**哈
+
+//: __整行都是粗体__
+
+//: ![Icon for playground](http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%8D%95%E4%B8%AAicon%E5%B0%8F%E5%9B%BE%E6%A0%87%E7%B4%A0%E6%9D%90&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=-1&cs=186644176,893448658&os=2124344169,310612890&pn=85&rn=1&di=125177508940&ln=1000&fr=&fmq=1438079515125_R&ic=undefined&s=undefined&se=&sme=&tab=0&width=&height=&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&gsm=0&objurl=http%3A%2F%2Fdown.tutu001.com%2Fd%2Ffile%2F20110101%2F3ee346c025e10ca4dc60e0b67a_560.jpg"我是tips")
