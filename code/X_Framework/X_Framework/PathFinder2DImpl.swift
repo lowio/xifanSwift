64f592139a0932d2453bb91b223cef7eeb7d9662
xifanGame <xifanGame@163.com>
2015-11-10 16:55:34 +0800

    ….

---
+++ code/X_Framework/X_Framework/PathFinder2DImpl.swift
@@ -0,0 +1,33 @@
+//
+//  PathFinder2DImpl.swift
+//  X_Framework
+//
+//  Created by xifanGame on 15/11/10.
+//  Copyright © 2015年 yeah. All rights reserved.
+//
+
+import Foundation
+
+//MARK: == PFinderPosition2D ==
+public struct PFinderPosition2D: Hashable
+{
+    //x, y
+    var x, y: Int;
+    
+    init(x: Int, y: Int)
+    {
+        self.x = x;
+        self.y = y;
+    }
+    
+    public var hashValue: Int{return self.x ^ self.y;}
+}
+public func ==(lsh: PFinderPosition2D, rsh: PFinderPosition2D) -> Bool{return lsh.x == rsh.x && lsh.y == rsh.y;}
+
+
+////MARK: == PFinderDataSource2D ==
+//public protocol PFinderDataSource2D: PFinderDataSource
+//{
+//    //position type
+//    typealias Position = PFinderPosition2D;
+//}
